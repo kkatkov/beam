0e0a4b2581c04d3ca5c6b9167a1a0a1988796492
vader <vrvaderom@gmail.com>
2018-08-28 00:18:11 +0300

    added config file

--- beam.py
+++ beam.py
@@ -1,10 +1,16 @@
 import logging
 import random
 import time
+import ConfigParser
+
+
+Config = ConfigParser.RawConfigParser()
+Config.read('beam.cfg')
 
 # create logger with 'spam_application'
 logger = logging.getLogger('spam_application')
 logger.setLevel(logging.DEBUG)
+
 # create file handler which logs even debug messages
 fh = logging.FileHandler('spam.log')
 fh.setLevel(logging.DEBUG)
@@ -23,6 +29,26 @@ logger.addHandler(ch)
 messages = ["hello", "test","woohoo","pizdabol"]
 
 
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+loglevel = ConfigSectionMap("default")['loglevel']
+print loglevel
+
 while True:
-  logger.info(messages[random.randint(0, 3)])
+  if loglevel == 'error':
+    logger.error(messages[random.randint(0, 3)])
+  else:
+    logger.info(messages[random.randint(0, 3)])
   time.sleep(3)
