027d46135a3825d880c4dc60dc8a15289390c764
vader <vrvaderom@gmail.com>
2018-08-27 23:53:45 +0300

    initial commit

---
+++ beam.py
@@ -0,0 +1,28 @@
+import logging
+import random
+import time
+
+# create logger with 'spam_application'
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+messages = ["hello", "test","woohoo","pizdabol"]
+
+
+while True:
+  logger.info(messages[random.randint(0, 3)])
+  time.sleep(3)
